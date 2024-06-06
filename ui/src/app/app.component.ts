be935902512b45c407fe8322bfecccdcfd24e55a
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-06-06 14:04:59 +0500

    Contact GetALL

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TourService } from './services/tours/tour.service';
+import { ContactserviceService } from './services/Contact/contactservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,9 @@ import { TourService } from './services/tours/tour.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  constructor(private _service : TourService){}
+  constructor(private _service : ContactserviceService){}
   ngOnInit(): void {
-    this._service.getTourById(1).
+    this._service.GetAllContact().
     subscribe(
       (data)=>
       console.log(data)
--- ui/src/app/services/Contact/contactservice.service.ts
+++ ui/src/app/services/Contact/contactservice.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { ContactModel } from './interfaces/contact-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactserviceService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  apiUrl = environment.apiUrl
+
+  GetAllContact(): Observable<ContactModel[]>{
+    return this.http.get<ContactModel[]>(`${this.apiUrl}Contact/GetAllContacts`);
+  }
 }
